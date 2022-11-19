5835d70e4a6203d1d02171b66dd262f317c9135c
anylots <842567494@qq.com>
2022-11-19 23:37:46 +0800

    add testnet config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,12 +5,18 @@ require("@nomicfoundation/hardhat-toolbox");
 // testing the frontend.
 require("./tasks/faucet");
 
+const { GOERLI_PRIVATE_KEY = '', ALCHEMY_PROJECT_ID = "" } = process.env;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.10",
   networks: {
     hardhat: {
       chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_PROJECT_ID}`,
+      accounts: [GOERLI_PRIVATE_KEY]
     }
   }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,27 @@
-const Campaign = require("../artifacts/contracts/Campaign.sol/Campaign.json")
+const usdt_address = '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9';
 
 
 // This is a script for deploying your contracts. You can adapt it to deploy
 // yours, or create new ones.
 async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    await deployer.getAddress()
+  );
   
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const AD3Hub = await ethers.getContractFactory("AD3Hub");
+  const ad3Hub = await AD3Hub.deploy();
+  await ad3Hub.deployed();
+  console.log("ad3Hub address:", ad3Hub.address);
+
+  await ad3Hub.setPaymentToken(usdt_address);
 }
+
+
 
 main()
   .then(() => process.exit(0))
