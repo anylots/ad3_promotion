6dfefb821873532fb8c5fd42dd116fa0a4c115a2
swarosky44 <swarosky44@gmail.com>
2023-02-16 02:45:32 +0800

    feat: 调整

--- contracts/AD3Hub.sol
+++ contracts/AD3Hub.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Campaign.sol";
 
-
 /**
  * @title AD3Hub contract
  * @dev Main point of interaction with an Ad3 protocol's campaign manage
@@ -14,192 +13,262 @@ import "./Campaign.sol";
  *   # GetCampaignAddress
  * - Owner can:
  *   # PayfixFee
- *   # Pushpay 
+ *   # Pushpay
  *   # Withdraw
  * - All admin functions are callable by the deployer
  *
  * @author Ad3
  **/
 contract AD3Hub is Ownable {
-    //make the transfer lower gas-used and more safety.
-    using SafeTransferLib for IERC20;
+  //make the transfer lower gas-used and more safety.
+  using SafeTransferLib for IERC20;
 
-    /*//////////////////////////////////////////////////////////////
-                                EVENTS
-    //////////////////////////////////////////////////////////////*/
-
-    event CreateCampaign(address indexed advertiser, uint256 indexed totalBudget);
-
-    event PayFixFee(address indexed advertiser);
-
-    event Pushpay(address indexed advertiser);
-
-    event Withdraw(address indexed advertiser);
-
-
-    /*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                                 STORAGE
     //////////////////////////////////////////////////////////////*/
 
-    // Campaign budget token, used to prepare campaign funds and pay to kol and user.
-    address public _paymentToken;
+  // createCampaign & claimRward transaction fee ratio
+  uint256 public _ratio;
 
-    // The ecdsa signer used to verify claim for user prizes
-    address public _trustedSigner;
+  // The ecdsa signer used to verify claim for user prizes
+  address public _trustedSigner;
 
-    // Logical implementation of campaign
-    address private _campaignImpl;
+  // Logical implementation of campaign
+  address private _campaignImpl;
 
-    // Mapping from Advertiser address to campaign addresses
-    mapping(address => mapping(uint64 => address)) private campaigns;
+  // Mapping from Advertiser address to campaign addresses
+  mapping(address => mapping(uint64 => address)) private campaigns;
 
-    // Mapping from campaign address to the lastest campaignId, 
-    // campaignId should be incremented from 1.
-    mapping(address => uint64) private campaignIds;
+  // Mapping from campaign address to the lastest campaignId,
+  // campaignId should be incremented from 1.
+  mapping(address => uint64) private campaignIds;
 
-
-    /*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                         ADVERTISER OPERATIONS
     //////////////////////////////////////////////////////////////*/
 
-    /**
-     * @dev Create an campaign with Minimal Proxy.
-     * @param kols The list of kol
-     * @param totalBudget The amount of campaign
-     * @param userFee amount to be awarded to each user
-     **/
-    function createCampaign(AD3lib.kol[] calldata kols, uint256 totalBudget, uint256 userFee) external 
-    returns(address instance){
-        require(kols.length > 0, "kols is empty.");
+  /**
+   * @dev Create an campaign with Minimal Proxy.
+   * @param cpaBonusBudget cpa task bonus total budget
+   * @param taskBonusBudget normal task bonus total budget
+   * @param cpaPaymentToken cpa bonus token contract address
+   * @param taskPaymentToken task bonus token contract address
+   **/
+  function createCampaign(
+    uint256 cpaBonusBudget,
+    uint256 taskBonusBudget,
+    address cpaPaymentToken,
+    address taskPaymentToken
+  ) external returns (address instance) {
+    require(cpaBonusBudget >= 0, "AD3Hub: cpa bonus budget less than zero.");
+    require(taskBonusBudget >= 0, "AD3Hub: task bonus budget less than zero.");
+    require(
+      cpaPaymentToken != address(0),
+      "AD3Hub: cpa token address is zero."
+    );
+    require(
+      taskPaymentToken != address(0),
+      "AD3Hub: task token address is zero."
+    );
 
-        bytes20 impl = bytes20(_campaignImpl);
+    bytes20 impl = bytes20(_campaignImpl);
 
-        /// @solidity memory-safe-assembly
-        assembly{
-            // Load free memory point.
-            let proxy :=mload(0x40)
-            // Copying runtime code into memory, get the calldata, prepare input and output parmeter.
-            mstore(proxy, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
-            // Copying impl address into memory.
-            mstore(add(proxy, 0x14), impl)
-            // Delegating the call.
-            mstore(add(proxy, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
-            instance := create(0, proxy, 0x37)
-        }
-        require(instance != address(0), "ERC1167: campaign create failed.");
-
-        // init campaign   
-        Campaign(instance).init(kols, userFee, _paymentToken, _trustedSigner);
-        // init amount
-        IERC20(_paymentToken).safeTransferFrom(
-            msg.sender,
-            instance,
-            totalBudget
-        );
-
-        // save campaign to mapping
-        uint64 lastest = campaignIds[msg.sender];
-        lastest++;
-        campaigns[msg.sender][lastest] = instance;
-        campaignIds[msg.sender] = lastest;
-        emit CreateCampaign(msg.sender, totalBudget);
+    /// @solidity memory-safe-assembly
+    assembly {
+      // Load free memory point.
+      let proxy := mload(0x40)
+      // Copying runtime code into memory, get the calldata, prepare input and output parmeter.
+      mstore(
+        proxy,
+        0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000
+      )
+      // Copying impl address into memory.
+      mstore(add(proxy, 0x14), impl)
+      // Delegating the call.
+      mstore(
+        add(proxy, 0x28),
+        0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000
+      )
+      instance := create(0, proxy, 0x37)
     }
+    require(instance != address(0), "ERC1167: campaign create failed.");
 
-    /*//////////////////////////////////////////////////////////////
+    // init campaign
+    Campaign(instance).init(
+      cpaPaymentToken,
+      taskPaymentToken,
+      _trustedSigner,
+      _ratio
+    );
+    // init cpa amount
+    IERC20(cpaPaymentToken).safeTransferFrom(
+      msg.sender,
+      instance,
+      cpaBonusBudget
+    );
+    // init task amount
+    IERC20(taskPaymentToken).safeTransferFrom(
+      msg.sender,
+      instance,
+      taskBonusBudget
+    );
+
+    // save campaign to mapping
+    uint64 lastest = campaignIds[msg.sender];
+    lastest++;
+    campaigns[msg.sender][lastest] = instance;
+    campaignIds[msg.sender] = lastest;
+  }
+
+  /*//////////////////////////////////////////////////////////////
                            OWNER OPERATIONS
     //////////////////////////////////////////////////////////////*/
 
-    /**
-     * @dev Withdraw the remaining funds to advertiser.
-     * @param advertiser The campaign's creater or owner
-     * @param campaignId index in advertiser's campaign list
-     **/
-    function withdraw(address advertiser, uint64 campaignId) external onlyOwner{
-        require(advertiser != address(0), "AD3Hub: advertiser is zero address.");
+  /**
+   * @dev Withdraw the remaining funds to advertiser.
+   * @param advertiser The campaign's creater or owner
+   * @param campaignId index in advertiser's campaign list
+   **/
+  function withdraw(address advertiser, uint64 campaignId) external onlyOwner {
+    require(advertiser != address(0), "AD3Hub: advertiser is zero address.");
 
-        require(
-            campaigns[advertiser][campaignId] != address(0),
-            "AD3Hub: No such campaign"
-        );
+    require(
+      campaigns[advertiser][campaignId] != address(0),
+      "AD3Hub: No such campaign"
+    );
 
-        bool withdrawSuccess = Campaign(campaigns[advertiser][campaignId]).withdraw(advertiser);
-        require(withdrawSuccess, "AD3: withdraw failured.");
-        emit Withdraw(advertiser);
-    }
+    bool withdrawSuccess = Campaign(campaigns[advertiser][campaignId]).withdraw(
+      advertiser
+    );
+    require(withdrawSuccess, "AD3: withdraw failured.");
+  }
 
+  /**
+   * @dev Set trustedSigner of campaign.
+   * @param trustedSigner address of trustedSigner
+   **/
+  function setTrustedSigner(address trustedSigner) external onlyOwner {
+    require(
+      trustedSigner != address(0),
+      "AD3Hub: trustedSigner is zero address."
+    );
+    _trustedSigner = trustedSigner;
+  }
 
-    /**
-     * @dev Set Payment Token of campaign.
-     * @param token address of token
-     **/
-    function setPaymentToken(address token) external onlyOwner{
-        require(token != address(0), "AD3Hub: paymentToken is zero address.");
-        _paymentToken = token;
-    }
+  /**
+   * @dev Get trustedSigner of campaign.
+   * @return trustedSigner address of trustedSigner
+   **/
+  function getTrustedSigner() external view returns (address) {
+    return _trustedSigner;
+  }
 
-    /**
-     * @dev Get Payment Token of campaign.
-     * @return token address of token
-     **/
-    function getPaymentToken() external view returns (address){
-        return _paymentToken;
-    }
+  /**
+   * @dev Set tranasation fee ratio of campagin
+   * @param ratio transaction fee ratio
+   */
+  function setRatio(ratio) external onlyOwner {
+    _ratio = ratio;
+  }
 
-    /**
-     * @dev Set trustedSigner of campaign.
-     * @param trustedSigner address of trustedSigner
-     **/
-    function setTrustedSigner(address trustedSigner) external onlyOwner{
-        require(trustedSigner != address(0), "AD3Hub: trustedSigner is zero address.");
-        _trustedSigner = trustedSigner;
-    }
+  /**
+   * @dev Get tranasation fee ratio of campagin.
+   * @return ratio transaction fee ratio
+   **/
+  function getTrustedSigner() external view returns (address) {
+    return _ratio;
+  }
 
-    /**
-     * @dev Get trustedSigner of campaign.
-     * @return trustedSigner address of trustedSigner
-     **/
-    function getTrustedSigner() external view returns (address){
-        return _trustedSigner;
-    }
+  /**
+   * @dev Set address of campaignImpl.
+   * @param campaign address of campaignImpl
+   **/
+  function setCampaignImpl(address campaign) external onlyOwner {
+    require(campaign != address(0), "AD3Hub: campaignImpl is zero address.");
+    _campaignImpl = campaign;
+  }
 
-    /**
-     * @dev Set address of campaignImpl.
-     * @param campaign address of campaignImpl
-     **/
-    function setCampaignImpl(address campaign) external onlyOwner{
-        require(campaign != address(0), "AD3Hub: campaignImpl is zero address.");
-        _campaignImpl = campaign;
-    }
-
-
-    /*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                         PUBLIC OPERATIONS
     //////////////////////////////////////////////////////////////*/
 
-    /**
-     * @dev get Address of Campaign
-     * @param advertiser The address of the advertiser who create campaign
-     **/
-    function getCampaignAddress(address advertiser, uint64 campaignId) public view returns(address){
-        require(advertiser != address(0), "AD3Hub: advertiser is zero address.");
-        return campaigns[advertiser][campaignId];
-    }
+  /**
+   * @dev claim cpa user prize.
+   * @param advertiser
+   * @param campaignId
+   * @param amount
+   * @param _signature
+   **/
+  function claimCpaReward(
+    address advertiser,
+    address campainId,
+    uint256 amount,
+    bytes memory _signature
+  ) external {
+    require(advertiser != address(0), "AD3Hub: advertiser is zero address.");
+    require(
+      campaigns[advertiser][campaignId] != address(0),
+      "AD3Hub: No such campaign"
+    );
+    Campaign(campaigns[advertiser][campaignId]).claimCpaReward(
+      amount,
+      _signature
+    );
+  }
 
-    /**
-     * @dev get Address list of Campaign
-     * @param advertiser The address list of the advertiser who create campaign
-     **/
-    function getCampaignAddressList(address advertiser) public view returns(address[] memory){
-        require(advertiser != address(0), "AD3Hub: advertiser is zero address.");
-        uint64 lastest = campaignIds[advertiser];
-        address[] memory campaignList;
-        if(lastest == 0){
-            return campaignList;
-        }
-        campaignList = new address[](lastest);
-        for(uint64 i =0; i<lastest; ++i){
-            campaignList[i] = campaigns[advertiser][i+1];
-        }
-        return campaignList;
+  /**
+   * @dev claim cpa user prize.
+   * @param advertiser
+   * @param campaignId
+   * @param amount
+   * @param _signature
+   **/
+  function claimTaskReward(
+    address advertiser,
+    address campainId,
+    uint256 amount,
+    bytes memory _signature
+  ) external {
+    require(advertiser != address(0), "AD3Hub: advertiser is zero address.");
+    require(
+      campaigns[advertiser][campaignId] != address(0),
+      "AD3Hub: No such campaign"
+    );
+    Campaign(campaigns[advertiser][campaignId]).claimTaskReward(
+      amount,
+      _signature
+    );
+  }
+
+  /**
+   * @dev get Address of Campaign
+   * @param advertiser The address of the advertiser who create campaign
+   **/
+  function getCampaignAddress(
+    address advertiser,
+    uint64 campaignId
+  ) public view returns (address) {
+    require(advertiser != address(0), "AD3Hub: advertiser is zero address.");
+    return campaigns[advertiser][campaignId];
+  }
+
+  /**
+   * @dev get Address list of Campaign
+   * @param advertiser The address list of the advertiser who create campaign
+   **/
+  function getCampaignAddressList(
+    address advertiser
+  ) public view returns (address[] memory) {
+    require(advertiser != address(0), "AD3Hub: advertiser is zero address.");
+    uint64 lastest = campaignIds[advertiser];
+    address[] memory campaignList;
+    if (lastest == 0) {
+      return campaignList;
     }
+    campaignList = new address[](lastest);
+    for (uint64 i = 0; i < lastest; ++i) {
+      campaignList[i] = campaigns[advertiser][i + 1];
+    }
+    return campaignList;
+  }
 }
--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -2,10 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-import {IERC20} from "./interfaces/IERC20.sol";
-import {SafeTransferLib} from "./libs/SafeTransferLib.sol";
-import "./libs/AD3lib.sol";
-
+import { IERC20 } from "./interfaces/IERC20.sol";
+import { SafeTransferLib } from "./libs/SafeTransferLib.sol";
+import { ECDSA } from "./libs/ECDSA.sol";
 
 /**
  * @title Campaign contract
@@ -17,153 +16,175 @@ import "./libs/AD3lib.sol";
  * @author Ad3
  **/
 contract Campaign {
-    //make the transfer lower gas-used and more safety.
-    using SafeTransferLib for IERC20;
+  //make the transfer lower gas-used and more safety.
+  using SafeTransferLib for IERC20;
 
-    /*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                                 EVENTS
     //////////////////////////////////////////////////////////////*/
 
-    event ClaimPrize(address indexed user, uint256 indexed amount);
+  event CreateCampaign(address indexed advertiser);
 
+  event ClaimCpaReward(address indexed user);
 
-    /*//////////////////////////////////////////////////////////////
+  event ClaimTaskReward(address indexed user);
+
+  /*//////////////////////////////////////////////////////////////
                                 STORAGE
     //////////////////////////////////////////////////////////////*/
 
-    // campaign only init once.
-    bool private _initialized;
+  // campaign only init once.
+  bool private _initialized;
 
-    // budget amount for per user.
-    uint256 public _userFee;
+  // budget amount for per user.
+  uint256 public _userFee;
 
-    // address of ad3hub contract.
-    address private _ad3hub;
+  // address of ad3hub contract.
+  address private _ad3hub;
 
-    // campaign budget token.
-    address public _paymentToken;
+  // campaign cpa budget token.
+  address public _cpaPaymentToken;
 
-    // the ecdsa signer used to verify claim for user prizes.
-    address private _trustedSigner;
-    
-    // the kol info saved in the storage.
-    mapping(address => AD3lib.kol) private _kolStorages;
+  // campaign task budget token.
+  address public _taskPaymentToken;
 
-    // the account has claimed.
-    mapping(address => bool) hasClaimed;
+  // the ecdsa signer used to verify claim for user prizes.
+  address private _trustedSigner;
 
-    uint256 public unlockTime;
+  // the kol info saved in the storage.
+  mapping(address => AD3lib.kol) private _kolStorages;
 
-    uint256 public unlockBlock;
+  // the account cpa reward has claimed.
+  mapping(address => bool) claimedCpaAddress;
 
+  // the account task reward has claimed.
+  mapping(address => bool) claimedTaskAddress;
 
+  uint256 public unlockTime;
 
-    /*//////////////////////////////////////////////////////////////
+  uint256 public unlockBlock;
+
+  /*//////////////////////////////////////////////////////////////
                            OWNER OPERATIONS
     //////////////////////////////////////////////////////////////*/
 
-    /**
-     *@dev Throws if called by any account other than the Ad3Hub.
-     */
-    modifier onlyAd3Hub() {
-        require(
-            msg.sender == _ad3hub,
-            "The caller must be ad3hub."
-        );
-        _;
-    }
+  /**
+   *@dev Throws if called by any account other than the Ad3Hub.
+   */
+  modifier onlyAd3Hub() {
+    require(msg.sender == _ad3hub, "The caller must be ad3hub.");
+    _;
+  }
 
-    /**
-     * @dev Constructor.
-     * @param kols The list of kol
-     * @param userFee amount to be awarded to each user
-     * @param paymentToken address of paymentToken
-     **/
-    function init(
-        AD3lib.kol[] memory kols,
-        uint256 userFee,
-        address paymentToken,
-        address trustedSigner
-    ) public {
-        require(_initialized == false, "AD3: campaign is already initialized.");
-        _initialized = true;
+  /**
+   * @dev Constructor.
+   * @param kols The list of kol
+   * @param userFee amount to be awarded to each user
+   * @param paymentToken address of paymentToken
+   **/
+  function init(
+    address cpaPaymentToken,
+    address taskPaymentToken,
+    address trustedSigner
+  ) public {
+    require(_initialized == false, "AD3: campaign is already initialized.");
+    _initialized = true;
 
-        _ad3hub = msg.sender;
-        _userFee = userFee;
-        _paymentToken = paymentToken;
-        _trustedSigner = trustedSigner;
-        for (uint64 i = 0; i < kols.length; i++) {
-            AD3lib.kol memory kol = kols[i];
-            require(kol.kolAddress != address(0), "AD3: kol_address is zero address.");
-            require(kol.fixedFee > 0, "AD3: kol fixedFee <= 0");
-            require(kol.ratio >= 0, "AD3: kol ratio < 0");
-            require(kol.ratio <= 100, "AD3: kol ratio > 100");
+    _ad3hub = msg.sender;
+    _cpaPaymentToken = cpaPaymentToken;
+    _taskPaymentToken = taskPaymentToken;
+    _trustedSigner = trustedSigner;
 
-            _kolStorages[kol.kolAddress] = kol;
-        }
-    }
+    emit CreateCampaign(msg.sender)
+  }
 
+  /**
+   * @dev Withdraw the remaining funds to advertiser.
+   * @param advertiser The campaign's creater or owner
+   **/
+  function withdraw(address advertiser) public onlyAd3Hub returns (bool) {
+    require(
+      block.timestamp >= unlockTime + 30 days,
+      "Funds cannot be withdrawn yet"
+    );
+    unlockBlock = block.number + 172800;
+    require(block.number >= unlockBlock, "Funds cannot be withdrawn yet");
 
-    /**
-     * @dev Withdraw the remaining funds to advertiser.
-     * @param advertiser The campaign's creater or owner
-     **/
-    function withdraw(address advertiser) public onlyAd3Hub returns (bool) {
-        require(block.timestamp >= unlockTime + 30 days, "Funds cannot be withdrawn yet");
-        unlockBlock = block.number + 172800;
-        require(block.number >= unlockBlock, "Funds cannot be withdrawn yet");
+    uint256 balance = IERC20(_paymentToken).balanceOf(address(this));
 
-        uint256 balance = IERC20(_paymentToken).balanceOf(address(this));
+    IERC20(_paymentToken).safeTransfer(advertiser, balance);
 
-        IERC20(_paymentToken).safeTransfer(advertiser, balance);
+    return true;
+  }
 
-        return true;
-    }
-
-    /*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                            USER OPERATIONS
     //////////////////////////////////////////////////////////////*/
 
-    /**
-     * @dev claim user prize.
-     * @param signature ECDSA signature of prize
-     * @param amount The campaign's creater or owner
-     **/
-    function claimUserPrize(AD3lib.PrizeSignature calldata signature, uint256 amount) external {
-        require(hasClaimed[msg.sender] == false, "Repeated claim.");
-        require(amount <= _userFee, "Amount invalid.");
-        
-        address signer = ecrecover(_createMessageDigest(address(this), msg.sender, amount), signature.v, signature.r, signature.s);
-        require(signer != address(0), "PrizeSigner is zero address.");
-        require(_trustedSigner == signer, "PrizeSignature invalid.");
-
-        hasClaimed[msg.sender] = true;
-        IERC20(_paymentToken).safeTransfer(msg.sender, amount);
-
-        emit ClaimPrize(msg.sender, amount);
-    }
-
-    /**
-     * @dev _createMessageDigest.
-     * @param _campaign campaign address
-     * @param _user user address
-     * @param _amount prize amount for claim
-     **/
-    function _createMessageDigest(address _campaign, address _user, uint256 _amount) internal pure returns (bytes32) {
-        return keccak256(
-            abi.encodePacked(
-                "\x19Ethereum Signed Message:\n32",
-                keccak256(abi.encodePacked(_campaign, _user, _amount))
-            )
+  /**
+   * @dev claim cpa user prize.
+   * @param amount the cpa task reward amount
+   * @param signature ECDSA signature of cpa reward
+   **/
+  function claimCpaReward(uint256 amount, bytes memory _signature) external {
+    require(
+      claimedCpaAddress[msg.sender] == true,
+      "AD3Hub: Repeated claim reward."
     );
-}  
+    require(amount <= 0, "Amount invalid.");
 
-    /**
-     * @dev Query campaign remain balance.
-     **/
-    function remainBalance() public view returns (uint256) {
-        uint256 balance = IERC20(_paymentToken).balanceOf(address(this));
-        return balance;
-    }
+    bytes _ethSignedMesssageHash = ECDSA.toEthSignedMessageHash(
+      keccak256(abi.encodePacked(address(this), msg.sender, amount))
+    );
 
+    require(
+      verify(_ethSignedMessageHash, _signature),
+      "PrizeSignature invalid."
+    );
+
+    claimedCpaAddress[msg.sender] = true;
+    IERC20(_cpaPaymentToken).safeTransfer(msg.sender, amount);
+
+    emit ClaimCpaReward(msg.sender);
+  }
+
+  /**
+   * @dev claim task user prize.
+   * @param amount the task reward amount
+   * @param signature ECDSA signature of task reward
+   **/
+  function claimTaskReward(uint256 amount, bytes memory _signature) external {
+    require(
+      claimedTaskAddress[msg.sender] == true,
+      "AD3Hub: Repeated claim reward."
+    );
+    require(amount <= 0, "AD3Hub: Amount invalid.");
+
+    bytes _ethSignedMessageHash = ECDSA.toEthSignedMessageHash(
+      keccak256(abi.encodePacked(address(this), msg.sender, amount))
+    );
+
+    require(
+      verify(_ethSignedMessageHash, _signature),
+      "AD3Hub: PrizeSignature invalid."
+    );
+
+    claimedTaskAddress[msg.sender] = true;
+    IERC20(_taskPaymentToken).safeTransfer(msg.sender, amount);
+
+    emit ClaimTaskReward(msg.sender);
+  }
+
+  /**
+   * @dev Query campaign remain balance.
+   **/
+  function remainCpaBonusBalance() public view returns (uint256) {
+    uint256 balance = IERC20(_cpaPaymentToken).balanceOf(address(this));
+    return balance;
+  }
+
+  function remainTaskBonusBlance() public view returns (uint256) {
+    uint256 balance = IERC20(_taskPaymentToken).balanceOf(address(this));
+    return balance;
+  }
 }
